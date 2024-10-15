a718b92e1e0ae31dae205462568c3ab03f17525d
Anshul Soni <anshulsoni2022@gmail.com>
2024-10-15 15:52:14 +0530

    testing

--- components/ThemeToggle.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-const ThemeToggle = () => {
-  // Remove toggle functionality
-  return null; // No toggle needed for dark theme only
-};
-
-export default ThemeToggle;
